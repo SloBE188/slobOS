ca1b5de692453bc67ae0bc7bf69b10623f0ec514
SloBE188 <nils.burkard@outlook.com>
2023-12-15 18:40:52 +0100

    Bootloader2: set segment registers

--- boot.asm
+++ boot.asm
@@ -1,9 +1,21 @@
-ORG 0x7c00
+ORG 0
 
-
+jmp 0x7c0:start
 [BITS 16]
 
-_start:
+start:
+
+    ;Hier werden nun die Segment register manuell gesetzt, dass sie nicht random vom BIOS gesetzt werden. 
+    ;Das SS muss anders gesetzt werden als die restlichen.
+    cli ;Clear Interrupts
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti ;Enable Interrupts
+
     mov si, message
     call print
     jmp $
@@ -26,7 +38,3 @@ message: db 'Welcome to CentOS', 0
 
 times 510- ($ - $$) db 0
 dw 0xAA55       ;boot signature
-
-
-;nasm -f bin ./boot.asm -o ./boot.bin
-;qemu-system-x86_64 -hda ./boot.bin
