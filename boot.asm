bccc7c962dfd664bf0924b6330adee427c75f9ec
SloBE188 <nils.burkard@outlook.com>
2023-12-15 19:04:28 +0100

    Implementing interrupts in Real Mode

--- boot.asm
+++ boot.asm
@@ -9,6 +9,23 @@ _start:
 times 33 db 0
 start:
     jmp 0x7c0:step2 ;Hier wird das CS register mit 0x7c0 ersetzt und zum Label "step2" gesprungen
+
+handle_zero:
+    mov ah, 0eh
+    mov al, 'A'
+    mov bx, 0x00
+    int 0x10
+    iret
+
+handle_one:
+    mov ah, 0eh
+    mov al, 'V'
+    mov bx, 0x00
+    int 0x10
+    iret
+
+
+
 step2:
     ;Hier werden nun die Segment register manuell gesetzt, dass sie nicht random vom BIOS gesetzt werden. 
     ;Das SS muss anders gesetzt werden als die restlichen.
@@ -20,6 +37,17 @@ step2:
     mov ss, ax
     mov sp, 0x7c00
     sti ;Enable Interrupts
+
+    ;4 Bytes interrupt 0
+    mov word[ss:0x00], handle_zero
+    mov word[ss:0x02], 0x7c0
+
+    ;4 Bytes interrupt 1
+    mov word[ss:0x04], handle_one
+    mov word[ss:0x06], 0x7c0
+
+    ;interrupt 1 aufrufen (handle_one)
+    int 1
 
     mov si, message
     call print
