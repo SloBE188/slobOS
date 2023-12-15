7ca2ca38cd1e7ee7f59cc03c5534abd5d9fb6981
SloBE188 <nils.burkard@outlook.com>
2023-12-15 18:24:04 +0100

    Bootloader1

---
+++ boot.asm
@@ -0,0 +1,32 @@
+ORG 0x7c00
+
+
+[BITS 16]
+
+_start:
+    mov si, message
+    call print
+    jmp $
+
+print:
+   mov bx, 0
+.loop:
+   lodsb
+   cmp al, 0
+   je .done
+   call print_char
+    jmp .loop
+.done:
+    ret
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
+message: db 'Welcome to CentOS', 0
+
+times 510- ($ - $$) db 0
+dw 0xAA55       ;boot signature
+
+
+;nasm -f bin ./boot.asm -o ./boot.bin
+;qemu-system-x86_64 -hda ./boot.bin
