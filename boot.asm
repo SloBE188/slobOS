d4b1198dfd5bdac270f4394877cbadf21dfe77a8
SloBE188 <nils.burkard@outlook.com>
2023-12-27 16:53:56 +0100

    Switching to Protected Mode

--- boot.asm
+++ boot.asm
@@ -1,5 +1,10 @@
-ORG 0
+ORG 0x7c00
 [BITS 16]
+
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
 
 ;Das Label "_start" braucht es wegen dem BIOS Parameter Block
 _start:
@@ -8,7 +13,7 @@ _start:
 
 times 33 db 0
 start:
-    jmp 0x7c0:step2 ;Hier wird das CS register mit 0x7c0 ersetzt und zum Label "step2" gesprungen
+    jmp 0:step2
 
 
 
@@ -16,53 +21,65 @@ step2:
     ;Hier werden nun die Segment register manuell gesetzt, dass sie nicht random vom BIOS gesetzt werden. 
     ;Das SS muss anders gesetzt werden als die restlichen.
     cli ;Clear Interrupts
-    mov ax, 0x7c0
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
     sti ;Enable Interrupts
 
 
-    mov ah, 2   ;READ SECTOR COMMAND
-    mov al, 1   ;ONE SECTOR TO READ
-    mov ch, 0   ;Cylinder low eight bits
-    mov cl, 2   ;Head sector two
-    mov dh, 0   ;Head number
-    mov bx, buffer
-    int 0x13
-    jc error
+.load_protected:
+    cli 
+    lgdt[gdt_descriptor]    ;loads GDT
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
-    mov si, buffer
-    call print
-    jmp $
+;GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
 
-error:
-    mov si, error_message
-    call print
+;offset 0x8
+gdt_code:       ;CS
+    dw 0xffff   ;Segment limit first 0-15 bits
+    dw 0        ;Base first 0-15 bits
+    db 0        ;Base 16-23 bits
+    db 0x9a     ;Access byte
+    db 11001111b;High 4 bit flags and the low 4 bit flags
+    db 0        ;Base 24-31 bits
 
-    jmp $
+;offset 0x10
+gdt_data:       ;DS,SS,ES,FS,GS
+    dw 0xffff   ;Segment limit first 0-15 bits
+    dw 0        ;Base first 0-15 bits
+    db 0        ;Base 16-23 bits
+    db 0x92     ;Access byte
+    db 11001111b;High 4 bit flags and the low 4 bit flags
+    db 0        ;Base 24-31 bits
 
-print:
-   mov bx, 0
-.loop:
-   lodsb
-   cmp al, 0
-   je .done
-   call print_char
-    jmp .loop
-.done:
-    ret
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start -1
+    dd gdt_start
 
 
-error_message: db 'Failed to load Sector'
+;Der Kernel läuft nun in einem 32bit mode (Protected Mode), wir können das BIOS nicht mehr nutzen.
+[BITS 32]
+load32:         
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
+jmp $
 
 times 510- ($ - $$) db 0
 dw 0xAA55       ;boot signature
-
-buffer:
