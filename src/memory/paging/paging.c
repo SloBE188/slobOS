27e61fc0c8798004ee26fe8b169bf851ee4c9d35
SloBE188 <nils.burkard@outlook.com>
2023-12-30 13:49:44 +0100

    paging comments

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -4,44 +4,53 @@
 void paging_load_directory(uint32_t* directory);
 
 static uint32_t* current_directory = 0;
+
+// Funktion zum Erstellen eines 4GB großen Paging-Bereichs.
 struct paging_4gb_chunk* paging_new_4gb(uint8_t flags)
 {
-    //Creates the memory for the page directory.
+    // Erstellt Speicher für das Page Directory. Jeder Eintrag im Verzeichnis zeigt auf eine Page Table.
     uint32_t* directory = kzalloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE);
     int offset = 0;
     for (int i = 0; i < PAGING_TOTAL_ENTRIES_PER_TABLE; i++)
     {
-        //Creates the memory for all the page tables of the directorys created above.
+        // Erstellt Speicher für alle Page Tables im oben erstellten Verzeichnis.
         uint32_t* entry = kzalloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE);
         for (int b = 0; b < PAGING_TOTAL_ENTRIES_PER_TABLE; b++)
         {
+            // Jeder Eintrag in der Tabelle wird mit der Basisadresse der Seite und den übergebenen Flags konfiguriert.
             entry[b] = (offset + (b * PAGING_PAGE_SIZE)) | flags;
         }
         offset += (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE);
+        // Der Verzeichniseintrag verweist auf die Page Table und wird als beschreibbar markiert.
         directory[i] = (uint32_t)entry | flags | PAGING_IS_WRITEABLE;
     }
 
+    // Erstellt einen Chunk für die 4GB große Paging-Struktur und speichert das Verzeichnis darin.
     struct paging_4gb_chunk* chunk_4gb = kzalloc(sizeof(struct paging_4gb_chunk));
     chunk_4gb->directory_entry = directory;
     return chunk_4gb;
 }
 
+// Funktion zum Wechseln des aktuellen Page Directories.
 void paging_switch(uint32_t* directory)
 {
     paging_load_directory(directory);
     current_directory = directory;
 }
 
+// Gibt das Page Directory des übergebenen 4GB Paging-Chunks zurück.
 uint32_t* paging_4gb_chunk_get_directory(struct paging_4gb_chunk* chunk)
 {
     return chunk->directory_entry;
 }
 
+// Überprüft, ob eine Adresse page-aligned ist.
 bool paging_is_aligned(void* addr)
 {
     return ((uint32_t)addr % PAGING_PAGE_SIZE) == 0;
 } 
 
+// Ermittelt die Indexe für das Directory und die Tabelle basierend auf einer virtuellen Adresse.
 int paging_get_indexes(void* virtual_address, uint32_t* directory_index_out, uint32_t* table_index_out)
 {
     int res = 0;
@@ -51,12 +60,14 @@ int paging_get_indexes(void* virtual_address, uint32_t* directory_index_out, uin
         goto out;
     }  
 
+    // Berechnet den Index im Page Directory und in der Page Table.
     *directory_index_out = ((uint32_t)virtual_address / (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE));
-    *table_index_out = ((uint32_t) virtual_address % (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE) / PAGING_PAGE_SIZE);
+    *table_index_out = ((uint32_t)virtual_address % (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE) / PAGING_PAGE_SIZE);
 out:
     return res;
 }
 
+// Setzt einen Wert in der Page Table für eine gegebene virtuelle Adresse.
 int paging_set(uint32_t* directory, void* virt, uint32_t val)
 {
     if (!paging_is_aligned(virt))
@@ -72,6 +83,7 @@ int paging_set(uint32_t* directory, void* virt, uint32_t val)
         return res;
     }
 
+    // Zugriff auf den entsprechenden Eintrag in der Page Table und Setzen des Wertes.
     uint32_t entry = directory[directory_index];
     uint32_t* table = (uint32_t*)(entry & 0xfffff000);
     table[table_index] = val;
