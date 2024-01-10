9670815422b0ae1e87341c227e50bad379591d11
SloBE188 <nils.burkard@outlook.com>
2024-01-10 09:57:39 +0100

    impemented fat16 filesystem

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -5,13 +5,34 @@ ORG 0x7c00
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
+jmp short start
+nop
 
-;Das Label "_start" braucht es wegen dem BIOS Parameter Block
-_start:
-    jmp short start
-    nop
 
-times 33 db 0
+; FAT16 Header
+OEMIdentifier           db 'CENTOS  '   ;string must be 8 bits
+BytesPerSector          dw 0x200
+SectorsPerCluster       db 0x80
+ReservedSectors         dw 200
+FATCopies               db 0x02
+RootDirEntries          dw 0x40
+NumSectors              dw 0x00
+MediaType               db 0xF8
+SectorsPerFat           dw 0x100
+SectorsPerTrack         dw 0x20
+NumberOfHeads           dw 0x40
+HiddenSectors           dd 0x00
+SectorsBig              dd 0x773594
+
+; Extended BPB (Dos 4.0)
+DriveNumber             db 0x80
+WinNTBit                db 0x00
+Signature               db 0x29
+VolumeID                dd 0xD105
+VolumeIDString          db 'PEACHOS BOO'
+SystemIDString          db 'FAT16   '   ;string must be 8 bits
+
+
 start:
     jmp 0:step2
 
