9b843490938954c77c94ee4c836eea41eacc18c6
SloBE188 <nils.burkard@outlook.com>
2024-01-24 12:17:10 +0100

    implemented fat16 fread function

--- src/kernel.c
+++ src/kernel.c
@@ -133,13 +133,17 @@ void kernel_main()
 
 
 
-    char buf[20];
-    int fd = fopen("0:/hello2.txt", "r");
-    strcpy(buf, "hello!");
+    int fd = fopen("0:/hello.txt", "r");
     if (fd)
     {
-        print("We opened hello.txt\n");
+        print("\nWe opened hello.txt\n");
+        char buf[14];
+        fread(buf, 13, 1, fd);
+        buf[13] = 0x00;
+        print(buf);
     }
+    while(1) {}
+
 
     /*PATH PARSER TESTING
         struct path_root* root_path = pathparser_parse("0:/bin/shell.exe", NULL);
