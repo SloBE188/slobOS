c54739086720544303b40e437fc3538e2b427470
SloBE188 <nils.burkard@outlook.com>
2024-04-26 10:07:47 +0200

    implementing elf 4

--- src/loader/formats/elfloader.c
+++ src/loader/formats/elfloader.c
@@ -130,3 +130,139 @@ int elf_validate_loaded(struct elf_header *header)
     return (elf_valid_signature(header) && elf_valid_class(header) && elf_valid_encoding(header) && elf_has_program_header(header)) ? SLOBOS_ALL_OK : -EINVARG;
 }
 
+
+// Verarbeitet den Programmheader für den Typ PT_LOAD (mein elf loader unterstützt keinen anderen typ, nur static)
+int elf_process_phdr_pt_load(struct elf_file *elf_file, struct elf32_phdr *phdr)
+{
+
+    // Hier erfolgt die eigentliche Adressberechnung und Speicherzuweisung basierend auf dem Programmheader
+    if (elf_file->virtual_base_address >= (void*) phdr->p_vaddr || elf_file->virtual_base_address == 0x00)
+    {
+        elf_file->virtual_base_address = (void*) phdr->p_vaddr;
+        elf_file->physical_base_address = elf_memory(elf_file)+phdr->p_offset;
+    }
+    
+    unsigned int end_virtual_address = phdr->p_vaddr + phdr->p_filesz;
+    if (elf_file->virtual_end_address <= (void*)(end_virtual_address) || elf_file->virtual_end_address == 0x00) 
+    {
+        elf_file->virtual_end_address = (void*) end_virtual_address;
+        elf_file->physical_end_address = elf_memory(elf_file)+phdr->p_offset+phdr->p_filesz;
+    }
+    
+
+    return 0;
+}
+
+
+// Verarbeitet einen spezifischen Programmheader
+int elf_process_pheader(struct elf_file *elf_file, struct elf32_phdr *phdr)
+{
+    int res = 0;
+    switch (phdr->e_type)
+    {
+    case PT_LOAD:
+        res = elf_process_phdr_pt_load(elf_file, phdr);
+    break;
+    }
+}
+
+
+// Verarbeitet alle Programmheader der ELF-Datei und gibt jeden einzelnen program header vom elf file der funktion "elf_process_pheader" weiter, 
+//welche dann jeden einzelnen programm header spezifisch verarbeitet
+int elf_process_pheaders(struct elf_file *elf_file)
+{
+    int res = 0;
+    struct elf_header *header = elf_header(elf_file);   //returns the elf header (start of a elf file)
+    for (int i = 0; i < header->e_phnum ; i++)  //loops threw all the elf program headers with the e_phnum variable(stores the count of the program headers)
+    {
+        struct elf32_phdr *phdr = elf_program_header(header, i);        //gets every program header of the elf file given
+        res = elf_process_pheader(elf_file, phdr);      //processes every single program header
+        if (res < 0)
+        {
+            break;
+        }
+        
+    }
+    
+
+    return res;
+}
+
+
+// Verarbeitet die geladene ELF-Datei
+int elf_process_loaded(struct elf_file *elf_file)
+{
+    int res = 0;
+    struct elf_header *header = elf_header(elf_file);
+    res = elf_validate_loaded(header);          //checks if the elf file is supported by my elf loader
+    if (res < 0)
+    {
+        goto out;
+    }
+    
+    res = elf_process_pheaders(elf_file);       //processes program headers (gives control to elf_process_pheaders function which then gives it to elf_process_pheaders and then to elf_process_phdr_pt_load)
+    if (res < 0)
+    {
+        goto out;
+    }
+    
+
+out:
+    return res;
+}
+
+/*Das ist die Hauptfunktion die für das Laden eines elf files zuständig ist. Sie lädt eine ELF-Datei von einer Datei.
+Sie reserviert speicher auf dem heap für die elf_file struktur und dann noch für das ganze elf file. D
+ann ruft sie die funktion "elf_process_loaded" auf, welche dann "elf_process_pheaders" aufruft, die dann "elf_process_pheader" aufruft und am schluss ruft elf_process_pheader dann die funktion "elf_process_phdr_pt_load" auf*/
+int elf_load(const char *filename, struct elf_file **file_out)
+{
+    struct elf_file *elf_file = kzalloc(sizeof(struct elf_file));
+    int fd = 0;
+
+    int res = fopen(filename, "r");
+    if (res <= 0)
+    {
+        goto out;
+    }
+    
+    fd = res;       //fd is the filepointer now
+    struct file_stat stat;
+    res = fstat(fd, &stat);     //gets the size of the elf file
+    if (res <= 0)
+    {
+        goto out;
+    }
+    
+    elf_file->elf_memory = kzalloc(stat.filesize);      //reserviert speicher auf dem heap for the whole elf file
+    res = fread(elf_file->elf_memory, stat.filesize, 1, fd);    //reading the entire file into memory
+    if (res < 0)
+    {
+        goto out;
+    }
+    
+    res = elf_process_loaded(elf_file); //processes a loaded elf file
+
+    if (res < 0)
+    {
+        goto out;
+    }
+    
+    *file_out = elf_file;       //sets the pointer value of the file_out param so it is the elf_file i worked with in this function
+
+out:
+    fclose(fd);
+    return res;
+}
+
+//closes the elf file(frees all the memory in the heap)
+void elf_close(struct elf_file *file)
+{
+    if (!file)
+    {
+        return;
+    }
+    
+    kfree(file->elf_memory);
+    kfree(file);
+}
+
