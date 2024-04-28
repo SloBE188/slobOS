04ebb2773563c08bf1d15ee8c7eed314ca80bf48
SloBE188 <nils.burkard@outlook.com>
2024-04-28 21:45:36 +0200

    forgot to restore the stack with add esp, 4 fix

--- programs/stdlib/src/slobos.asm
+++ programs/stdlib/src/slobos.asm
@@ -11,6 +11,7 @@ print:
     push dword[ebp+8]   ;+8 because the "push ebp" was 4 bytes and the return address (message) from the person who called this function is also 4 bytes
     mov eax, 1 ; command print syscall
     int 0x80    ;interrupt 80 is syscall
+    add esp, 4
     pop ebp
     ret
 
