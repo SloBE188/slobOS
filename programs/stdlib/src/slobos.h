d6325608be4dae1b62deb181acfa1fb4dd7b6932
SloBE188 <nils.burkard@outlook.com>
2024-05-01 11:50:42 +0200

    fixing some typos

--- programs/stdlib/src/slobos.h
+++ programs/stdlib/src/slobos.h
@@ -1,6 +1,8 @@
 #ifndef SLOBOS_H
 #define SLOBOS_H
 
+#include <stddef.h>
+
 void print(const char *message);
 int getkey();
 void* slobos_malloc(size_t size);
--- src/isr80h/heap.c
+++ src/isr80h/heap.c
@@ -6,7 +6,7 @@
 void* isr80h_command4_malloc(struct interrupt_frame* frame)
 {
 
-    size_t size = (int)task_get_stack_item(task_current(), 0)
+    size_t size = (int)task_get_stack_item(task_current(), 0);
     return process_malloc(task_current() ->process, size);      //i use "task_current()->process" because thats the one that gets executed here and now, process_current would give me the process im looking at back which would be false
 
 }
